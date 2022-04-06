6740ef8926168fe8641ebfc4eeef6a84a4b4d406
Jasper Westra <jasper.westra@live.nl>
2022-04-06 14:00:48 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthChangeEvent, createClient, Session, SupabaseClient } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  private supabase: SupabaseClient;
+  session = undefined
+
+  constructor() {
+    this.supabase = createClient('https://oodnhkxnztaljdvabstx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vZG5oa3huenRhbGpkdmFic3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDcwMDMwNTgsImV4cCI6MTk2MjU3OTA1OH0._MAnGYebBxc1CtvDfs0EE2J-ZOxjyzAkpn11qkxFIpE');
+    this.supabase.auth.onAuthStateChange((evt, session) => {
+      this.session = session;
+    });
+  }
+
+  signIn() {
+    return this.supabase.auth.signIn({
+      provider: 'keycloak'
+    });
+  }
+
+  signOut() {
+    return this.supabase.auth.signOut();
+  }
 }
